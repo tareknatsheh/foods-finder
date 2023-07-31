3db3dbb3f80833474530f92188ee8bc215bf3c78
sarab5000 <32688470+sarab5000@users.noreply.github.com>
2023-07-31 06:29:41 +0300

    fixed PORT hardcoded bug

--- api/src/server.ts
+++ api/src/server.ts
@@ -21,6 +21,6 @@ app.get('*', (req, res) => {
 
 const PORT: number = parseInt(process.env.PORT) || 5000;
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     console.log(`Server is up and running at port ${PORT}`);
 })
